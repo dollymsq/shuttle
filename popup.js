566497125a3ea3663fa829de47e8114e6bc779fe
Dolly Ma <dollietmsq@gmail.com>
2015-10-22 20:07:08 -0400

    not working right now with notifications

--- popup.js
+++ popup.js
@@ -9,10 +9,10 @@ chrome.runtime.onInstalled.addListener(function() {
     // With a new rule ...
       chrome.declarativeContent.onPageChanged.addRules([
         {
-          // That fires when a page's URL contains a 'g' ...
+          // That fires when a page's URL contains the 'shuttle.brown.edu/user' ...
           conditions: [
             new chrome.declarativeContent.PageStateMatcher({
-               pageUrl: { urlContains: "shuttle.brown.edu/user" },
+               pageUrl: { urlContains: "sso.brown.edu/" },
             })
           ],
           // And shows the extension's page action.
@@ -21,3 +21,31 @@ chrome.runtime.onInstalled.addListener(function() {
       ]);
     });
   });
+
+// request permission on page load
+document.addEventListener('DOMContentLoaded', function () {
+  if (Notification.permission !== "granted")
+    Notification.requestPermission();
+});
+
+function notifyMe() {
+  if (!Notification) {
+    alert('Desktop notifications not available in your browser. Try Chromium.'); 
+    return;
+  }
+
+  if (Notification.permission !== "granted")
+    Notification.requestPermission();
+  else {
+    var notification = new Notification('Notification title', {
+      icon: 'http://cdn.sstatic.net/stackexchange/img/logos/so/so-icon.png',
+      body: "Hey there! You've been notified!",
+    });
+
+    notification.onclick = function () {
+      window.open("http://stackoverflow.com/a/13328397/1269037");      
+    };
+    
+  }
+
+}
